7a6f78453fcfee4f1218fb99f59de1bc4709c7d7
akwendeh <opemiporachael@gmail.com>
2022-09-15 16:23:45 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,14 +14,15 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (inc1 =1; inc1 <= size; inc1++)
+		for (inc1 = 1; inc1 <= size; inc1++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
 			for (inc2 = 0; inc2 < inc1; inc2++)
-			{putchar('#');
+			{
+				putchar('#');
 			}
 			if (inc1 == size)
 			{
@@ -30,4 +31,5 @@ void print_triangle(int size)
 			putchar('\n');
 		}
 	}
+	putchar('\n');
 }
